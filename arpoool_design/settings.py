22ba9836d3be41e1ee245da3b677ab2fdedf768e
dooweed <bydev2001@gmail.com>
2024-05-27 04:11:31 +0500

    Minor fixes

--- arpoool_design/settings.py
+++ arpoool_design/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 ROOT_URLCONF = "arpoool_design.urls"
