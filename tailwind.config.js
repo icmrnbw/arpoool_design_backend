287ae09f2094264225a66662a7487c0aa054933d
maybecacao <amojetkakao@gmail.com>
2024-05-28 18:49:59 +0500

    minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  content: [
-    './templates/**/*.html',
-    './static/js/**/*.js',
-    // Add other paths to your templates and static files
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './templates/*.html',
+        './templates/chunks/*.html',
+        './static/js/**/*.js',
+        // Add other paths to your templates and static files
+    ],
+    theme: {
+        extend: {
+          colors: {
+            'purple-70%': "rgba(144, 0, 255, 0.7)",
+            'black-30%': "rgba(0, 0, 0, 0.3)",
+            'contact': "rgba(41, 41, 43, 0.2)",
+          },
+          screens: {
+            '1180px': '1180px',
+            '1000px': '1000px',
+            '900px': '900px',
+          }
+        },
+    },
+    plugins: [],
 };
